b565b0be93f794637fb191a9678137774771e113
Francis Hage <francis.hage@code.nl>
2025-11-05 23:37:15 +0100

    Fix ICE server configuration format for Trystero

--- src/network.js
+++ src/network.js
@@ -26,13 +26,11 @@ const config = {
   appId: 'https://qedpxdusplakbbdmkqke.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZHB4ZHVzcGxha2JiZG1rcWtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MTc1NDgsImV4cCI6MjA3NzM5MzU0OH0.pM8IdSC8oFb1IxDYm84yLBigG-CiRTcK8A58XtGqLb0',
-  rtcConfig: {
-    iceServers: [
-      {urls: 'stun:stun.l.google.com:19302'},
-      {urls: 'stun:stun1.l.google.com:19302'}
-    ],
-    iceCandidatePoolSize: 10
-  }
+  iceServers: [
+    {urls: 'stun:stun.l.google.com:19302'},
+    {urls: 'stun:stun1.l.google.com:19302'}
+  ],
+  iceCandidatePoolSize: 10
 }
 
 /**
