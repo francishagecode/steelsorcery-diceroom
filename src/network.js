4f4c344ceb87c3a0c8355b3f32562f7d93c5c77a
Francis Hage <francis.hage@code.nl>
2025-11-05 23:32:32 +0100

    Increase ICE candidate pool size for Chrome compatibility

--- src/network.js
+++ src/network.js
@@ -30,7 +30,8 @@ const config = {
     iceServers: [
       {urls: 'stun:stun.l.google.com:19302'},
       {urls: 'stun:stun1.l.google.com:19302'}
-    ]
+    ],
+    iceCandidatePoolSize: 10
   }
 }
 
